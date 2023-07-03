891c88f95714e35612119978d449a383c3a684dc
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-07-03 01:30:55 -0300

    Versão com post /participantes completa

--- src/app.js
+++ src/app.js
@@ -1,23 +1,76 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+import dayjs from "dayjs";
+import joi from 'joi'
+import Joi from "joi";
 
 const server = express();
 server.use(cors());
 server.use(express.json())
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+mongoClient.connect()
+    .then(()=>db = mongoClient.db())
+    .catch((err)=>console.log(err.response))
 
 const participantes = [];
+const mensagens = []
 
-server.post('/participants',(req,res)=>{
+// PARTICIPANTS
+server.post('/participants',async(req,res)=>{
     const {name} = req.body;
-    if (participantes.includes(name)){
-        return res.sendStatus(409);
+
+    const partiScheme = Joi.object({
+        name: Joi.string().required()
+    })
+
+    const validation = partiScheme.validate(req.body, {abortEarly:false});
+
+    if (validation.error) return res.status(422);
+
+    const lastStatus = Date.now();
+    const novoparticpante = {name,lastStatus}
+    const nova_mensa = { 
+		from: name,
+		to: 'Todos',
+		text: 'entra na sala...',
+		type: 'status',
+		time: dayjs(lastStatus).format('HH:mm:ss')
     }
-    participantes.push(name);
-    res.sendStatus(201)
+
+    try{
+        if (await db.collection('participants').findOne({name:name})){
+            return res.sendStatus(409);
+        }
+
+        await db.collection('participants').insertOne(novoparticpante);
+        await db.collection('messages').insertOne(nova_mensa);
+        res.sendStatus(201);
+    }catch(err){
+        res.status(500).send(err.message)
+
+    }
 })
 
 server.get('/participants',(req,res)=>{
     res.send(participantes);
+})
+
+//MESSAGES
+server.post('/messages',(req,res)=>{
+    const {to, text,type}= req.body;
+    const nova_mensa = {
+        to: "Maria",
+        text: "oi sumida rs",
+        type: "private_message",
+        time: dayjs(Date.now()).format('HH:mm:ss')
+    }
+    res.sendStatus(201);
 })
 
 const PORT = 5000;
